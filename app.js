06c110258909e1aef6b05ac06e1b7684e31d7b0a
Denis Rawls <denrawls@gmail.com>
2018-06-03 15:23:11 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,107 @@
+const 	bodyParser 			= require("body-parser"),
+		express 			= require("express"),
+		expressSanitizer 	= require("express-sanitizer"),
+		mongoose 			= require("mongoose"),
+		methodOverride 		= require("method-override"),
+		moment				= require("moment"),
+		app 				= express();
+
+mongoose.connect("mongodb://localhost/justbeingd_blog");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
+// Mongoose Schema
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created : {type: Date, default: Date.now}
+}) 
+var Blog = mongoose.model("Blog", blogSchema)
+
+// Routes
+
+app.get("/", function(req, res){
+	res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log("ERROR!");
+		} else {
+			res.render("index", {blogs: blogs})
+		}
+	});
+});
+
+// Route New
+app.get("/blogs/new", function(req, res){
+	res.render("new");
+});
+
+//Route Create
+app.post("/blogs", function(req, res){
+	//create a blog
+	req.body.blog.body = req.sanitize(req.body.blog.body)
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err){
+			res.render("new");
+		} else {
+			//then, redirect tro the index
+			res.redirect("/blogs");
+		}
+	});
+});
+
+// Route Show
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("show", {blog: foundBlog});
+		}
+	});
+});
+
+//Route Edit
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("edit", {blog: foundBlog});
+		}
+	});
+});
+
+// Route Update
+app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body)
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs/" + req.params.id);
+		}
+	});
+});
+
+//Route Delete
+app.delete("/blogs/:id", function(req, res){
+	//destroy blog
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
+});
+
+app.listen(3000);
+console.log("The server is up on port 3000");
